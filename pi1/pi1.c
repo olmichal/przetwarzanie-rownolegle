0af75311a9f0e3572a194f136205b768002373b7
olmichal <olczefski@gmail.com>
2022-04-14 16:52:46 +0200

    Add exercise 1-5

---
+++ pi1/pi1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <time.h>
+#include <omp.h>
+
+long long num_steps = 1000000000;
+double step;
+
+int main()
+{
+	clock_t spstart, spstop;
+	double sswtime, sewtime;
+	double x, pi, sum=0.0;
+	int i;
+
+	sswtime = omp_get_wtime();
+	spstart = clock();
+
+	step = 1./(double)num_steps;
+
+	for (i=0; i<num_steps; i++) {
+		x = (i + .5)*step;
+		sum = sum + 4.0/(1.+ x*x);
+	}
+	
+	pi = sum*step;
+
+	spstop = clock();
+    sewtime = omp_get_wtime();
+    printf("------------------------------------------------\n");
+    printf("%15.12f wartosc liczby PI sekwencyjnie \n", pi);
+    printf("------------------------------------------------\n");
+    printf("Czas procesorów przetwarzania sekwencyjnego  %f sekund \n", ((double)(spstop - spstart)/CLOCKS_PER_SEC));
+    printf("------------------------------------------------\n");
+    printf("Czas trwania obliczen sekwencyjnych - wallclock %f sekund \n",  sewtime-sswtime);
+    printf("------------------------------------------------\n");
+    return 0;
+}
---
+++ pi3/pi3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <omp.h>
+
+long long num_steps = 1000000000;
+double step;
+
+int threads[3] = {16,8,1};
+
+int main() {
+    printf("------------------------------------------------\n");
+    double pswtime, pewtime;
+	int i;
+
+    step = 1./(double)num_steps;
+
+    for(i = 0; i < 3; i++) {
+        double sum=0.0;
+        omp_set_num_threads(threads[i]);
+        pswtime = omp_get_wtime();
+
+        #pragma omp parallel for
+        for(i = 0; i < num_steps; i++) {
+            double x = (i + .5)*step;
+            #pragma omp atomic
+            sum = sum + 4.0/(1.+ x*x);
+        }
+        double pi = sum*step;
+
+        pewtime = omp_get_wtime();
+        printf("%15.12f Wartosc liczby PI rownolegle dla %d wątków\n",pi, threads[i]);
+        printf("Czas trwania obliczen rownoleglych dla %d wątków - wallclock %f sekund \n", threads[i], pewtime-pswtime);
+        printf("------------------------------------------------\n");
+    }
+    return 0;
+}
